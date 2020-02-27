c346e654aab66d0f0f605eb59bd84dcaeac5906b
Abdul Haseeb <42298266+abdulhvseeb@users.noreply.github.com>
2020-02-27 18:36:50 +0530

    Add files via upload

---
+++ myApp/myApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myApp
+//
+//  Created by Abdul Haseeb on 26/02/20.
+//  Copyright © 2020 Abdul Haseeb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
